38f99467e445b221a3709ecb976e6adc93758e9b
jlnsharma <niranjanj478@gamil.com>
2023-03-16 06:42:28 +0000

    mutliplevalues-es6

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,9 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+
+function multiplyByFour(n) {
+  if (typeof n === typeof 8) {
+    return n * 4;
+  }
+}
+export { multiplyByFour, myArray, bulb };
---
+++ importMultipleValues.mjs
@@ -0,0 +1,4 @@
+import { multiplyByFour, myArray, bulb } from "./exportMultipleValues.mjs";
+console.log(multiplyByFour(4));
+console.log(myArray);
+console.log(bulb);
